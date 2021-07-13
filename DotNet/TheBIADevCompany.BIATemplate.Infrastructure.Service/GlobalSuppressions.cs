a1a166f13dc4b8403bcc60915dd13a785415355f
Gwenael Fillon <gwenael.fillon@safrangroup.com>
2021-07-13 18:43:38 +0200

    V3.4.0

---
+++ DotNet/TheBIADevCompany.BIATemplate.Infrastructure.Service/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+// <copyright file="GlobalSuppressions.cs" company="TheBIADevCompany">
+//     Copyright (c) TheBIADevCompany. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "This project depend on plateform infrastructure", Scope = "member", Target = "~M:TheBIADevCompany.BIATemplate.Infrastructure.Service.Repositories.LdapRepository.ConvertToUserDirectory(System.DirectoryServices.DirectoryEntry,System.String)~TheBIADevCompany.BIATemplate.Domain.UserModule.Aggregate.UserFromDirectory")]
