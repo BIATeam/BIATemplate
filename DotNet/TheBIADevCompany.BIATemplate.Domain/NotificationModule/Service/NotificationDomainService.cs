b298c0e77e754724718ea99c3177b592c3c4fbc5
Gwenael Fillon <Gwenael.fillon@free.fr>
2021-10-06 23:59:09 +0200

    bug in notiication if not user

--- DotNet/TheBIADevCompany.BIATemplate.Domain/NotificationModule/Service/NotificationDomainService.cs
+++ DotNet/TheBIADevCompany.BIATemplate.Domain/NotificationModule/Service/NotificationDomainService.cs
@@ -69,13 +69,18 @@ namespace TheBIADevCompany.BIATemplate.Domain.NotificationModule.Service
             if (this.userId != 0)
             {
                 this.permissions = (principal as BIAClaimsPrincipal).GetUserPermissions().ToList();
-                this.filtersContext.Add(
+            }
+            else
+            {
+                this.permissions = new List<string>();
+            }
+
+            this.filtersContext.Add(
                     AccessMode.Read,
                     new DirectSpecification<Notification>(n =>
                         n.SiteId == this.currentSiteId
                         && (n.NotifiedPermissions.Count == 0 || n.NotifiedPermissions.Any(r => this.permissions.Contains(r.Permission.Code)))
                         && (n.NotifiedUsers.Count == 0 || n.NotifiedUsers.Any(u => u.UserId == this.userId))));
-            }
         }
 
         /// <inheritdoc/>
